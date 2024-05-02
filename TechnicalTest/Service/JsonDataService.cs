6d954812642b8129dee84052fb5af4e9dab8e3d4
Lawrence Zhang <lawrencezhangjm@gmail.com>
2024-05-02 17:38:55 +1200

    Add logging to JsonDataService

--- TechnicalTest/Service/JsonDataService.cs
+++ TechnicalTest/Service/JsonDataService.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using Newtonsoft.Json;
 using TechnicalTest.Model;
+using Microsoft.Extensions.Logging;
 
 
 namespace TechnicalTest.Service
@@ -10,21 +13,37 @@ namespace TechnicalTest.Service
     public class JsonDataService
     {
         private readonly string _basePath;
+        private readonly ILogger<JsonDataService> _logger;
 
-        public JsonDataService(IWebHostEnvironment env)
+        public JsonDataService(IWebHostEnvironment env, ILogger<JsonDataService> logger)
         {
             _basePath = env.ContentRootPath;
+            _logger = logger;
         }
 
         public IEnumerable<PatientDetails> GetPatientData()
         {
-            string filePath = Path.Combine(_basePath, "data", "patientsdata.json");
-            using (var reader = new StreamReader(filePath))
+            try
             {
-                string json = reader.ReadToEnd();
-                return JsonConvert.DeserializeObject<List<PatientDetails>>(json);
+                string filePath = Path.Combine(_basePath, "data", "patientsdata.json");
+                using (var reader = new StreamReader(filePath))
+                {
+                    string json = reader.ReadToEnd();
+                    return JsonConvert.DeserializeObject<List<PatientDetails>>(json);
+                }
+            }
+            catch (JsonException ex)
+            {
+                _logger.LogError(ex, "Error parsing JSON: {Message}", ex.Message);
+                return Enumerable.Empty<PatientDetails>();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unexpected error occurred while accessing the data.");
+                return Enumerable.Empty<PatientDetails>();
             }
         }
+
     }
 }
 
